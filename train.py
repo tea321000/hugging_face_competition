64e39a42a8e75919f27353b27eaf67943c40c951
tea321000 <834914152@qq.com>
2021-09-22 17:01:20 +0800

    add fc head

--- train.py
+++ train.py
@@ -32,19 +32,110 @@ class OneflowDataloaderToPytorchDataset(Dataset):
         pt_data["token_type_ids"] = torch.tensor(of_data[3].numpy()).cuda()
         pt_data["attention_mask"] = torch.tensor(of_data[2].numpy()).cuda()
         label = torch.tensor(of_data[1].numpy()).cuda()
-        return pt_data, label
+        masked_lm_ids = torch.tensor(of_data[4].numpy()).cuda()
+        masked_lm_positions = torch.tensor(of_data[5].numpy()).cuda()
+        masked_lm_weights = torch.tensor(of_data[6].numpy()).cuda()
+        return pt_data, label, masked_lm_ids, masked_lm_positions, masked_lm_weights
+
+
+class BertPreTrainingHeads(nn.Module):
+    def __init__(self, hidden_size, vocab_size, hidden_act=nn.GELU()):
+        super().__init__()
+        self.predictions = BertLMPredictionHead(hidden_size, vocab_size, hidden_act)
+        self.seq_relationship = nn.Linear(hidden_size, 2)
+
+    def forward(self, sequence_output, pooled_output):
+        prediction_scores = self.predictions(sequence_output)
+        seq_relationship_scores = self.seq_relationship(pooled_output)
+        return prediction_scores, seq_relationship_scores
+
+
+class BertLMPredictionHead(nn.Module):
+    def __init__(self, hidden_size, vocab_size, hidden_act=nn.GELU()):
+        super().__init__()
+        self.transform = BertPredictionHeadTransform(hidden_size, hidden_act)
+
+        # The output weights are the same as the input embeddings, but there is
+        # an output-only bias for each token.
+        self.decoder = nn.Linear(hidden_size, vocab_size, bias=False)
+
+        self.output_bias = nn.Parameter(torch.zeros(vocab_size))
+
+        # Need a link between the two variables so that the bias is correctly resized with `resize_token_embeddings`
+        self.decoder.bias = self.output_bias
+
+    def forward(self, sequence_output):
+        sequence_output = self.transform(sequence_output)
+        sequence_output = self.decoder(sequence_output)
+        return sequence_output
+
+
+class BertPredictionHeadTransform(nn.Module):
+    def __init__(self, hidden_size, hidden_act=nn.GELU()):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.transform_act_fn = hidden_act
+        self.LayerNorm = nn.LayerNorm(hidden_size)
+
+    def forward(self, sequence_output):
+        sequence_output = self.dense(sequence_output)
+        sequence_output = self.transform_act_fn(sequence_output)
+        sequence_output = self.LayerNorm(sequence_output)
+        return sequence_output
 
 
 class MultilabelTrainer(Trainer):
-    def compute_loss(self, model, labels, inputs, return_outputs=False):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        mlm_criterion = nn.CrossEntropyLoss(reduction="none")
+
+        def get_masked_lm_loss(
+                logit_blob,
+                masked_lm_positions,
+                masked_lm_labels,
+                label_weights,
+                max_prediction_per_seq,
+        ):
+            # gather valid position indices
+            logit_blob = torch.gather(
+                logit_blob,
+                index=masked_lm_positions.unsqueeze(2).to(dtype=torch.int64).repeat(1, 1, 30522),
+                dim=1,
+            )
+            logit_blob = torch.reshape(logit_blob, [-1, 30522])
+            label_id_blob = torch.reshape(masked_lm_labels, [-1])
+
+            # The `positions` tensor might be zero-padded (if the sequence is too
+            # short to have the maximum number of predictions). The `label_weights`
+            # tensor has a value of 1.0 for every real prediction and 0.0 for the
+            # padding predictions.
+            pre_example_loss = mlm_criterion(logit_blob, label_id_blob.long())
+            pre_example_loss = torch.reshape(pre_example_loss, [-1, max_prediction_per_seq])
+            sum_label_weight = torch.sum(label_weights, dim=-1)
+            sum_label_weight = sum_label_weight / label_weights.shape[0]
+            numerator = torch.sum(pre_example_loss * label_weights)
+            denominator = torch.sum(label_weights) + 1e-5
+            loss = numerator / denominator
+            return loss
+
+        self.ns_criterion = nn.CrossEntropyLoss(reduction="mean")
+        self.masked_lm_criterion = get_masked_lm_loss
+
+    def compute_loss(self, model, cls, labels, id, pos, weight, max_prediction_per_seq, inputs, return_outputs=False):
         # labels = inputs["labels"]
         outputs = model(**inputs)
-        logits = outputs.pooler_output.argmax(-1)
-        loss_fct = nn.BCEWithLogitsLoss()
-        loss = loss_fct(logits.view(-1, 1).float(),
-                        labels.float())
-        # print(loss)
-        return (loss, outputs) if return_outputs else loss
+        # self.cls = BertPreTrainingHeads(hidden_size, vocab_size)
+        prediction_scores, seq_relationship_scores = cls(outputs.last_hidden_state, outputs.pooler_output)
+        next_sentence_loss = self.ns_criterion(
+            seq_relationship_scores.view(-1, 2), labels.long().view(-1)
+        )
+
+        masked_lm_loss = self.masked_lm_criterion(
+            prediction_scores, pos, id, weight, max_prediction_per_seq=max_prediction_per_seq
+        )
+
+        total_loss = next_sentence_loss + masked_lm_loss
+        return (total_loss, outputs) if return_outputs else total_loss
 
 
 if __name__ == '__main__':
@@ -137,8 +228,10 @@ if __name__ == '__main__':
 
     print("Creating Dataloader")
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    configuration = BertConfig(hidden_size=args.hidden_size, num_hidden_layers=args.num_hidden_layers, num_attention_heads= args.num_attention_heads, intermediate_size=args.intermediate_size)
-    model = BertModel(configuration)
+    configuration = BertConfig(hidden_size=args.hidden_size, num_hidden_layers=args.num_hidden_layers,
+                               num_attention_heads=args.num_attention_heads, intermediate_size=args.intermediate_size)
+    model = BertModel(configuration).cuda()
+    cls = BertPreTrainingHeads(args.hidden_size, args.vocab_size).cuda()
     print("pytorch model", model)
     dataset = OneflowDataloaderToPytorchDataset(args)
     trainer = MultilabelTrainer(model=model)
@@ -147,8 +240,8 @@ if __name__ == '__main__':
     # output = model(**encoded_input)
     for i in range(args.epochs):
         for batch in tqdm(dataset):
-            encoded_input.data, label = batch
-            trainer.compute_loss(model, label, encoded_input)
+            encoded_input.data, label, id, pos, weight = batch
+            trainer.compute_loss(model, cls, label, id, pos, weight, args.max_predictions_per_seq, encoded_input)
 
     # train_data_loader = OfRecordDataLoader(
     #     ofrecord_dir=args.ofrecord_path,
@@ -163,5 +256,3 @@ if __name__ == '__main__':
     # for epoch in range(args.epochs):
     #     for batch in train_data_loader:
     #         print(batch)
-
-
